2707bcdaef594882d1b461f04204f83d78aed49e
Gabriel Rocha Stabile <gabrielrostabile@outlook.com>
2025-01-11 18:42:11 +0000

    feat: initialize app

---
+++ README.md
@@ -0,0 +1,3 @@
+# MoveOn App
+
+Um aplicativo para ajudar pessoas a se exercitarem de forma divertida e saudável.
---
+++ android/app/src/main/kotlin/pt/ips/ests/move_on_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package pt.ips.ests.move_on_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
