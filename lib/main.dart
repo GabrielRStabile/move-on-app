b080b5b3a268c0478555a5a4f3afe49f1b81e525
Gabriel Rocha Stabile <gabrielrostabile@outlook.com>
2025-02-01 14:35:25 +0000

    feat: initialize Flutter bindings and update main function for async execution

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,9 @@ import 'package:move_on_app/data/services/health/health_service.dart';
 import 'package:move_on_app/di/dependency_injection.dart';
 import 'package:move_on_app/ui/core/move_on_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   di.registerAll();
 
   unawaited(di.get<IHealthService>().init());
